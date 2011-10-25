bde779f4db99f2b7d0694247d462e6c81a9f03de
Stanislav Gumeniuk <vigo@vigo.su>
2011-10-25 14:01:48 +0400

    test

---
+++ unix_hw/hw5/task3/index.py
@@ -0,0 +1,14 @@
+#encoding:utf8
+import math
+
+a = input("Enter number: ")
+#print a**2
+b = a/2 +1;
+
+for i in range (2,b):
+    for j in range (2,i):
+        c = a/b
+        d = a%b
+        
+
+
