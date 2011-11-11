a8e26d82369b1250a5d9dad41724ab06215c13b0
Stanislav Gumeniuk <vigo@vigo.su>
2011-11-11 15:05:55 +0400

    test6

--- test11/test11/6.py
+++ test11/test11/6.py
@@ -1,3 +1,20 @@
 __author__ = 'vigo@vigo.su'
 
-  
+import re
+
+p = re.compile('(ab[a-z]?b)',re.IGNORECASE)
+reg =  p.search("abnb")
+if reg:
+    print reg.group(),' ',reg.span()
+
+f = open('6.txt','r')
+s1 = f.readline()
+s2 = f.readline()
+fuu = int(f.readline());
+print fuu
+strs = []
+for i in xrange(0,fuu,1):
+    ss = f.readline()
+    strs.append(ss)
+    print strs[i]
+f.close()
