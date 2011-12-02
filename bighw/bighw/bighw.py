118c80fd3d3683fb858f978e849aa3620af61187
ViGo5190 <vigo@vigo.su>
2011-12-03 00:19:01 +0400

    +1 draw my tree

--- bighw/bighw/bighw.py
+++ bighw/bighw/bighw.py
@@ -10,20 +10,13 @@ import os
 
 import math
 
-def get_tree(tree=[u"/home/vigo/python/",], G=nx.Graph(), itr=0, max_itr=900):
-    point = tree.pop(0)
-    itr = itr + 1
-    sub_tree = [os.path.join(point, x) for x in os.listdir(point) if os.path.isdir(os.path.join(point, x)) and not is_hidden_dir(os.path.join(point, x))]
-    if sub_tree:
-        tree.extend(sub_tree)
-        G.add_edges_from(map(lambda b : (point, b), sub_tree))
-    if tree and itr <= max_itr:
-        return get_tree(tree, G, itr)
-    else:
-        return G
 
-def search_midle():
-    return 0
+
+
+#---------------------
+
+G=nx.Graph()
+
 
 def is2pow(xxk):
     #print 'k=',xxk
@@ -35,31 +28,76 @@ def is2pow(xxk):
 def low2pow(xxk):
     return int(math.log(xxk,2))
 
-def load_data(G=nx.Graph()):
+
+
+
+def print_pic(pic_n=0):
+    labels=dict((n,str(d['label'])) for n,d in G.nodes(data=True))
+    pos2=dict((n,(d['x'],d['y'])) for n,d in G.nodes(data=True))
+    pos=nx.graphviz_layout(G,prog='dot',args='-y')
+    print labels
+    print pos
+    print pos2
+    print G.nodes(data=True)[1][1]['x']
+    nx.draw(G, pos2, with_labels=True,labels=labels, node_color="blue", node_size=1000)
+
+    plt.savefig("edge_colormap"+str(pic_n)+".png")
+    
+
+    plt.show()
+    
+
+
+def load_data():
+    pic_n = 0
     h=nx
     f = open('input.txt','r')
     data = f.readlines()
     f.close()
+    mylen = len(data)
+    some = low2pow(len(data))+1
     xx = 0
     yy = 1.0
-
+    znak = 1;
     for i in xrange(0,len(data)/2,1):
         ii = long(data[i])
 
         
         print i+1,ii,
         iid = 1
+        parentx = 0;
         #nx.draw_networkx_nodes( G,pos=[(1/(i+1),1/(i+1))],nodelist=G.nodes() )
         if i>0:
             if i>2:
                 iid = long((i+1)/2)
+                parentx = float(G.nodes(data=True)[iid-1][1]['x'])
+                print ">",parentx,"<",float(G.nodes(data=True)[iid-1][1]['z']),
             else:
                 iid = 1
-            #iid = low2pow(i+1)
+                parentx = 0
 
+            #iid = low2pow(i+1)
+            #if i< (2**(low2pow(i+1)+1)/2):
+            #    print i,"-"
+            #    znak = -1
+            #else:
+            #    print i,"+"
+            znak*=-1
             print "parent=",iid,
+
+            #print G.nodes(data=True)[iid][1]['x']
+            #myxx = 1.0/+znak*myx;
+
+
+
+        if i>0:
+            G.add_node(i+1,label=ii,x=float((parentx + ((some-low2pow(i))*znak* (1.0/(low2pow(i+1)+1)) )   )),y=-1*low2pow(i+1),z=znak)
             G.add_edge(iid,i+1)
-        G.add_node(i+1,label=ii,y=1.0/(low2pow(i+1)+1),x =  low2pow(i+1))
+        else:
+            G.add_node(i+1,label=ii,x=0,y=0,z=znak)
+
+
+
         k=int(i)
         if is2pow(i+1):
             yy/=2
@@ -67,23 +105,10 @@ def load_data(G=nx.Graph()):
 
 
     
-    #pos = {0:(1,1)}
+    print_pic()
+    #labels=dict((n,str(d['z'])+" "+str(d['label'])+"  "+str(d['x'])+"  "+str(d['y'])) for n,d in G.nodes(data=True))
 
-    
-    #pos=nx.spring_layout(G)
-    #H = nx.balanced_tree(2,2,G)
 
-    #pos=nx.graphviz_layout(G,prog='dot',args='-x -y')
-
-    labels=dict((n,str(d['label'])+"  "+str(d['x'])+"  "+str(d['y'])) for n,d in G.nodes(data=True))
-    #pos=dict(())
-    pos=nx.graphviz_layout(G,prog='dot',args='-x')
-    #print pos
-    nx.draw(G, pos, with_labels=True,labels=labels, node_color="blue", node_size=1000)
-
-    plt.savefig("edge_colormap.png")
-
-    plt.show()
 
 
 def make_tree(G=nx.Graph()):
