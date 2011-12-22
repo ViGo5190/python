952d7f26d0b0444cde9125ded393f2b3c3cb1b19
ViGo5190 <vigo@vigo.su>
2011-12-22 12:31:28 +0400

    testing

--- bighw/bighw/bighw_hentai.py
+++ bighw/bighw/bighw_hentai.py
@@ -28,7 +28,8 @@ def low2pow(xxk):
     return 0
 def parentn(n):
     if n>0:
-        return long((n+1)/2)-1
+        return int(n+1)/2-1
+        #return low2pow(n+2)-2
     return 0
 
 def load_data():
@@ -37,21 +38,27 @@ def load_data():
     xx = 0
     yy = 1.0
     znak = 1;
-    iid = 1
+    iid = -1
     some = low2pow(len(data))
     for i in xrange(0,len(data),1):
         somedata = []
-        if i<1:
+        #if i<1:
+        #    somedata.append(data[i])
+        #    somedata.append(0)
+        #    somedata.append(0)
+        #else:
+        if True:
             somedata.append(data[i])
-            somedata.append(0)
-            somedata.append(0)
-        else:
-            somedata.append(data[i])
-            x = int((vertex[parentn(i)][1]  + 10*((some-low2pow(i-1))*znak* (1.0/(low2pow(i+1))) )))
+            if i<1:
+                x = 0
+            else:
+                x = int((vertex[parentn(i)][1]  + 50*((some-low2pow(i))*znak* (1.0/(low2pow(i+1))) )))
+
             somedata.append(x)
-            somedata.append(100*(low2pow(i-1))+100)
+            somedata.append(100*(low2pow(i+1)-1)+100)
             znak*=-1
-            print somedata[2]
+            print i, parentn(i), low2pow(i+1)-1, somedata[2]
+            #print somedata[2]
 
 
 
@@ -61,7 +68,7 @@ def load_data():
     w.write('graph:\n')
     w.write('dir:0; simp:1; eucl:1; int:0; ew:1; vw:0;\n')
     w.write('scroller:\n')
-    w.write('vdim:1000; hdim:1000; vlinc:10; hlinc:10; vpinc:50; hpinc:50;\n')
+    w.write('vdim:1000; hdim:1500; vlinc:10; hlinc:10; vpinc:50; hpinc:50;\n')
     ver = 'vertices:'+str(len(vertex))+';\n'
     w.write(ver)
     for i in xrange(0,len(vertex),1):
